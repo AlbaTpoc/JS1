c5a79f6ab907eebff40339b6317d9cb300591836
AlbaTpoc <ivan.leshenko@icloud.com>
2023-04-03 22:00:14 +0300

    added new hw

---
+++ main.js
@@ -0,0 +1,66 @@
+/*let age = Number(prompt(`Сколько вам лет?`));
+
+let up = 17;
+
+let result = age + up;
+
+alert(result)
+
+let a = 10;
+
+alert(a)
+
+let b = 20;
+
+alert(b)
+
+let yearPhone = ('Впервые аппарат был представлен 9 января 2007 г.');
+alert(yearPhone)
+
+let nameJs = ('JavaScript был создан за 10 дней автором Бренданом Айком')
+alert(nameJs)
+
+let math = ('Cуммa, разность, произведение и частное чисел 10 и 2');
+alert(math)
+let x = Number(10);
+let y = Number(2);
+
+alert(x + y)
+alert(x * y)
+alert(x - y)
+alert(x / y)
+
+let result = Number(2 ** 5);
+alert(result)
+
+let x = 1;
+x += 5;
+x -= 3;
+x *= 7;
+x /= 3;
+x ++ 1;
+x -+ 1;
+alert(x);
+
+let age = Number(prompt(`Сколько вам лет?`));
+
+alert(age)
+
+let a = 9;
+let b = 2;
+let result = Number(a % b);
+alert(result)
+
+const user = {
+	name: 'Anton',
+	isAdmin: true,
+	age: 22,
+    ['city of residence']: 'Madrid',
+};
+user.age = 25;
+delete user['city of residence'];
+let info = prompt('Какую информацию хотите узнать о пользователе?');
+alert(user[info])*/
+
+let userName = prompt('Как вас зовут?');
+alert('Привет,' + userName);
