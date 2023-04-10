edfb3c8f73dbca97336e10cbd8191eeca7d33dcc
AlbaTpoc <ivan.leshenko@icloud.com>
2023-04-10 15:24:45 +0300

    hw-4

--- main.js
+++ main.js
@@ -55,7 +55,7 @@ const user = {
 	name: 'Anton',
 	isAdmin: true,
 	age: 22,
-    ['city of residence']: 'Madrid',
+	['city of residence']: 'Madrid',
 };
 user.age = 25;
 delete user['city of residence'];
@@ -63,6 +63,160 @@ let info = prompt('Какую информацию хотите узнать о 
 alert(user[info])
 
 let userName = prompt('Как вас зовут?');
-alert('Привет,' + userName);*/
+alert('Привет,' + userName);
 
 
+let password = 'пароль';
+
+let pass = prompt('Введите пароль');
+
+(pass === 'пароль') ? alert('Пароль введен верно') : alert('Пароль введен неправильно');
+
+let c = (2);
+
+c > 0 && c < 10 ? console.log('Верно') : console.log('Неверно');
+
+let d = (3);
+
+let e = (12);
+
+(d || e) > 100 ? console.log('Верно') : console.log('Неверно');
+
+let a = 2;
+let b = 3;
+alert(a + b);
+
+let monthNumber = prompt('Введите номер месяц в году')
+
+switch (monthNumber) {
+	case '12':
+		console.log('зима');
+		break;
+	case '1':
+		console.log('зима');
+		break;
+	case '2':
+		console.log('зима');
+		break;
+	case '3':
+		console.log('весна');
+		break;
+	case '4':
+		console.log('весна');
+		break;
+	case '5':
+		console.log('весна');
+		break;
+	case '6':
+		console.log('лето');
+		break;
+	case '7':
+		console.log('лето');
+		break;
+	case '8':
+		console.log('лето');
+		break;
+	case '9':
+		console.log('осень');
+		break;
+	case '10':
+		console.log('осень');
+		break;
+	case '11':
+		console.log('осень');
+		break;
+	default:
+		case monthNumber > 13:
+			console.log('Такого месяца не существует');
+		break;
+}
+
+
+
+
+if (number % 2 == 0) {
+	alert('Число четное');
+} else {
+	alert('Число нечетное');
+}
+
+let number = prompt('Пожалуйста, введите любое число');
+
+number % 2 == 0 ? alert('Число четное') : alert('Число нечетное');
+*/
+
+// let detect = new MobileDetect(window.navigator.userAgent);
+
+// console.log("Mobile: " + detect.mobile());
+// console.log("Phone: " + detect.phone());
+// console.log("Tablet: " + detect.tablet());
+// console.log("OS: " + detect.os());
+// console.log("userAgent: " + detect.userAgent());
+// alert("Mobile: " + detect.mobile());
+// alert("Phone: " + detect.phone());
+// alert("Tablet: " + detect.tablet());
+// alert("OS: " + detect.os());
+// alert("userAgent: " + detect.userAgent());
+
+// return this._cache.os;
+// if (t.os('iOS')) {
+// 	alert('11111111111111')
+// }
+// if (t.version('Android')) {
+// 	alert('123123')
+// }
+
+// let Hi = 0;
+// while (Hi < 2) {
+// 	console.log('Привет');
+// 	Hi++;
+// }
+
+// let i = 0;
+
+// while (i < 6) {
+// 	console.log(i);
+// 	i++;
+// }
+
+// let i = 7;
+
+// while (i < 23) {
+// 	console.log(i);
+// 	i++;
+// };
+
+// let i = 0;
+// do {
+// 	console.log(i);
+// 	i++;
+// } while (i > 7 && i < 22);
+
+// let obj = {
+// 	'Коля': 200,
+// 	'Вася': 300,
+// 	'Петя': 400,
+// };
+
+// for (let key in obj) {
+// 	alert(`${key} — зарплата ${obj[key]} dollars`)
+// }
+
+// let n = 1000;
+// let num = 0;
+
+// while (n > 50) {
+// 	num++;
+// 	n = n / 2;
+// }
+
+// console.log(n);
+// console.log('Iterated ' + num + ' times!')
+
+for (let dayNumber = 1; dayNumber <= 31; dayhNumber += 7) {
+	if (dayNumber == 5) {
+		console.log('Сегодня пятница, ...-е число. Необходимо подготовить отчет.');
+	} else if (dayNumber !== 5 && dayNumber <= 5 && dayNumber >= 31) {
+		console.log('Весна');
+	}
+}
